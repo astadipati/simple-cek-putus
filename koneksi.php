85aac09014eee14cd275a5db4b6ebe6696296ee4
astadipati <asekbos@gmail.com>
2016-11-29 11:25:15 +0700

    Update koneksi.php
    edit sendiri ya sesuai koneksi teman2

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";	//nama host
-$user = "root";	//username phpMyAdmin
-$pass = "Kul0nuwun";	//password phpMyAdmin
-$dbname = "sipp311";	//nama database
+$user = "bosku";	//username 
+$pass = "passente";	//password 
+$dbname = "sipptuban";	//nama database
 
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($dbname, $koneksi) or die("Tidak ada database yang dipilih!");
